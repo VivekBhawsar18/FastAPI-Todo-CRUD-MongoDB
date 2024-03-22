db4ffac4ed5b729da1f0489269d2c78dcc619912
ValtechInnovations <138601237+ValtechInnovations@users.noreply.github.com>
2024-03-22 11:05:25 +0530

    Initial project setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id, "name": "John Doe"}
