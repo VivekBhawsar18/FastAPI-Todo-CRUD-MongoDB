a59fbb355819f3390f6d71012b88ea6743678d55
Vivek Bhawsar <138601237+ValtechInnovations@users.noreply.github.com>
2024-03-22 12:03:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, from FastAPI hosted on RENDER!"}
 
 
 @app.get("/users/{user_id}")
